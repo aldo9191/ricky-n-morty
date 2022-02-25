ff2806e34aceb33c3c86e7174ae23fe10536382f
David Ortiz <david.ortiz.p@gmail.com>
2022-02-25 12:28:32 -0500

    fix padding in card

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,22 +4,34 @@ import avatar from "../../src/freddy.jpeg";
 
 export const StyledCard = styled.div`
   width: 250px;
-  height: 380px;
+  height: 300px;
   border-radius: 5px;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-  padding: 20px;
+  padding: 0px;
   display: flex;
   gap: 5px;
   flex-direction: column;
   align-items: flex-start;
 `;
 
+export const Avatar = styled.img`
+  width: 100%;
+  height: 150px;
+  margin: 0 auto;
+`;
+
+export const TextSection = styled.div`
+  padding: 20px;
+`;
+
 export const Card = () => {
   return (
     <StyledCard>
-      <img src={avatar} alt="avatar" />
-      <h2>Rick Sanchez</h2>
-      <p>Human</p>
+      <Avatar src={avatar} alt="avatar" />
+      <TextSection>
+        <h2>Rick Sanchez</h2>
+        <p>Human</p>
+      </TextSection>
     </StyledCard>
   );
 };
