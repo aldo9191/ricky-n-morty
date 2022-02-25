0fea34ea6c52e7af5153ca9d9d2b5c2d43bce814
David Ortiz <david.ortiz.p@gmail.com>
2022-02-25 18:32:23 -0500

    fix name of Api folder

--- src/Components/Characters.js
+++ src/Components/Characters.js
@@ -1,32 +1,30 @@
 import styled from "styled-components";
-import { getCharacters } from "../API/getCharacters";
+import { getCharacters } from "../Api/getCharacters";
 import { SearchBox, FilterBox } from "./Inputs";
-import { Card} from "./Card"
-import {useState, useEffect} from 'react';
+import { Card } from "./Card";
+import { useState, useEffect } from "react";
 
 const StyledGrid = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   gap: 20px;
   padding: 25px;
-  `
-
+`;
 
 export const Characters = () => {
   // getCharacters();
   let statusList = ["Alive", "Dead", "Zombie"];
   let genderList = ["Male", "Female", "Genderless"];
 
+  let [data, setData] = useState([]);
 
-  let [ data , setData] = useState([]);
-
-  useEffect(()=>{
+  useEffect(() => {
     // const fetchData = async ()=>{
     //   const data = await fetch('https://rickandmortyapi.com/api/character')
     //   const json = await data.json()
     //   setData(json)
     // }
-    
+
     // fetchData().catch(console.error)
 
     // (async() =>{
@@ -35,10 +33,9 @@ export const Characters = () => {
     // })()
 
     getCharacters()
-    .then(data => setData(data))
-    .catch(console.error)
-  },[])
-
+      .then((data) => setData(data))
+      .catch(console.error);
+  }, []);
 
   // console.log(typeof data.results)
 
@@ -49,16 +46,13 @@ export const Characters = () => {
       <FilterBox data={genderList} />
       <h2>Characters</h2>
       <StyledGrid>
-       {
-         (data.length !== 0)? (
-          data.results.map(character => (
-            <Card key={character.id} {...character}/>
+        {data.length !== 0 ? (
+          data.results.map((character) => (
+            <Card key={character.id} {...character} />
           ))
-        ): (
+        ) : (
           <p>No funciona</p>
-        )
-       }
-
+        )}
       </StyledGrid>
     </>
   );
